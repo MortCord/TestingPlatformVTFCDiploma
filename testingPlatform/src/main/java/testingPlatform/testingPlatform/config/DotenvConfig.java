60381aba5f877bfac80a0a6a6e65d7da1e5771fe
MortCord <ratynsky98@gmail.com>
2026-06-14 16:33:11 +0200

    Documentation added

--- testingPlatform/src/main/java/testingPlatform/testingPlatform/config/DotenvConfig.java
+++ testingPlatform/src/main/java/testingPlatform/testingPlatform/config/DotenvConfig.java
@@ -1,17 +1,17 @@
-package testingPlatform.testingPlatform.config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DotenvConfig {
-
-    @PostConstruct
-    public void loadEnv(){
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-    }
-
-}
+//package testingPlatform.testingPlatform.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DotenvConfig {
+//
+//    @PostConstruct
+//    public void loadEnv(){
+//        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+//
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//    }
+//
+//}
